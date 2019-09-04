01cf851cdaf8dee2c3447bf0c5790c14508780cf
markomih10 <mmihajlovic@csuchico.edu>
2019-09-04 16:17:48 -0700

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
+//Author: Marko Mihajlovic
 
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi,my name is Marko. What's yours?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
